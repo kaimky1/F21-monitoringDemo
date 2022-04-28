d9cc26a613b51303c75de9391a7e3d70371b43b2
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-04-28 10:05:17 -1000

    revused post function and added CSS

--- server/index.js
+++ server/index.js
@@ -23,9 +23,21 @@ app.get('/', (req,res) => {
 app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
-    students.push(name)
-    rollbar.log('Student added successfully', {author: 'Kai', type: 'manual entry'})
-    res.status(200).send(students)
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 
