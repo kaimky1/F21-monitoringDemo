00bbdf54914b898ac054f11aa67b9fcf9e4733c4
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-04-28 08:56:30 -1000

    Added post function

--- server/index.js
+++ server/index.js
@@ -13,11 +13,23 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
-
+let students = [];
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
     rollbar.info('html file served successfully.')
 })
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Kai', type: 'manual entry'})
+    res.status(200).send(students)
+})
+
+
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 app.listen(port, () => {
